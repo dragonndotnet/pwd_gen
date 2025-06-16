b25ab390386bad0c6d1a2311dfad1baf115e89f4
darkdotnet <daemonvibes@gmail.com>
2025-06-16 10:39:23 +0200

    bugfix:optimized sub-optimal code

--- pwd_gen.py
+++ pwd_gen.py
@@ -6,12 +6,7 @@ c_yellow = Fore.YELLOW
 c_green = Fore.GREEN
 c_reset = Fore.RESET
 
-#global vars
 pwd_nr = int(input(f"Enter {c_yellow}number{c_reset} of passwords: "))
-pwd_len = int(input(f"Enter {c_yellow}length{c_reset} of your password: "))
-withDigits = int(input(f"Enter how many {c_yellow}digits{c_reset}: "))
-withSpecial = int(input(f"Enter how many {c_yellow}special chars{c_reset}: "))
-char = string.ascii_letters + string.digits + string.punctuation
 
 
 #shuffle
@@ -21,18 +16,32 @@ def shuffle_pwd(pwd):
      return "".join(pwd_list)
 
 
-
 #loop
 for pwd_index in range(pwd_nr):
-        pwd = ''
+    pwd = ''
+    pwd_len = int(input(f"Enter {c_yellow}length{c_reset} of your password: "))
+    withDigits = input(f"Include {c_yellow}digits{c_reset}(y/n)?: ")
+    withSpecial = input(f"Include {c_yellow}special chars{c_reset}(y/n)?: ")
 
-        for digits_index in range(withDigits):
-            pwd = pwd + random.choice(string.digits)
+    # Convert to integer for calculations
+    withDigits_int = 1 if withDigits.lower() == 'y' else 0
+    withSpecial_int = 1 if withSpecial.lower() == 'y' else 0
 
-        for punctuation_index in range(withSpecial):
-            pwd = pwd + random.choice(string.punctuation)
+    char_pool = string.ascii_letters #Dynamic built based on user input
+    if withDigits == 'y':
+        char_pool += string.digits
+    if withSpecial == 'y':
+        char_pool += string.punctuation
 
-        for index in range(pwd_len - withDigits - withDigits):
-             pwd = pwd + random.choice(char)
-            #output
-        print(f'[{c_green}201{c_reset}]$-{c_green}{pwd_index}{c_reset}- PWD Generated: {shuffle_pwd(pwd)}')
+# Generate main part of password
+    for index in range(pwd_len - withDigits_int - withSpecial_int):
+        pwd += random.choice(char_pool)
+
+
+    if withDigits_int == 1:
+        pwd += random.choice(string.digits)
+
+
+    if withSpecial_int == 1:
+        pwd += random.choice(string.punctuation)
+    print(f'[{c_green}201{c_reset}]$-{c_green}{pwd_index}{c_reset}- PWD Generated: {shuffle_pwd(pwd)}')
